1d482fbfede8bb5f0ddd11358f91d80df42614ff
ijnamatsukaze75 <naromutajimav2@gmail.com>
2026-03-16 09:36:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
+
+});
